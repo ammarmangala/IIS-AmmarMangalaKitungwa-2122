1fbe797f63ce7684ba292d9c29fa355c548174fb
Ammar Mangala Kitungwa <ammar.mangala@gmail.com>
2022-02-20 18:32:41 +0100

    oefening Console Applicatie 211

---
+++ 01.IIP/ConsoleApp211/ConsoleApp211/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp211
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jaar invoeren : ");
+            int Jaar = int.Parse(Console.ReadLine());
+            if (((Jaar % 4 == 0) && (Jaar % 100 != 0)) || (Jaar % 400 == 0))
+            {
+                Console.WriteLine("{0} is een schrikkeljaar.", Jaar);
+            }
+            else 
+            { 
+                Console.WriteLine("{0} is geen schrikkeljaar.", Jaar);
+            }
+        }
+    }
+}
