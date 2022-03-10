7a99b6b728adf3d6c022d5029be65183205a3061
Ammar Mangala Kitungwa <ammar.mangala@gmail.com>
2022-03-10 23:09:31 +0100

    oefening 321 & 330

---
+++ 03.IIP/ConsoleApp321/ConsoleApp321/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.Write("Geef je naam op: ");
+string naam = Console.ReadLine();
+
+Console.Write("Geef telefoonnummer op (GSM) : ");
+string telNummer = Console.ReadLine(); 
+
+Console.Write("Geef je postcode op: ");
+string postcode = Console.ReadLine();
+
+// EERSTE TEKEN
+string eersteTekenVanNaam = naam.Substring(0, 1);
+string eersteLetterPaswoord = eersteTekenVanNaam.ToUpper();
+
+// TWEEDE TEKEN
+string tweedeTekenVanNaam = naam.Substring(1, 1);
+string tweedeLetterPaswoord = tweedeTekenVanNaam.ToLower();
+
+//ZONENUMMER
+string deelVanTelnummer = telNummer.Substring(1, 3);
+
+//POSTCODE
+string eersteCijferVanPostcode = postcode.Substring(0, 1);
+double machtverheffingPC = Math.Pow(Convert.ToInt32(eersteCijferVanPostcode), 2);
+
+//GEGENEREERD PASWOORD
+Console.WriteLine(tweedeLetterPaswoord + eersteLetterPaswoord + deelVanTelnummer + machtverheffingPC);
+
---
+++ 03.IIP/ConsoleApp330/ConsoleApp330/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1;
+Console.Write("Enter the 1st Number : ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2;
+Console.Write("Enter the 2nd Number : ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+int rem = 0;
+
+
+while (num2 > 0)
+{
+    rem = num1 % num2;
+    num1 = num2;
+    num2 = rem;
+}
+Console.WriteLine(" het grootste gemene deler is " + num1);
